333c24ee98353edc6a6c5a7d73959675ca13657c
PVS <sudarsanjuhi@gmail.com>
2025-08-07 08:55:53 +0000

    json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/event': 'http://10.24.158.207:8082',
+      '/event': 'http://10.24.1.10:8082',
     },
   },
 });
